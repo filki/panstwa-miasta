7185ec9c2bffe8b7bdce40edddd86cf0000b9fb3
filki <filipjanwojciech@gmail.com>
2026-05-18 18:53:09 +0200

    fix: unused imports ruff + undefined inlineJoin

--- static/js/socket.js
+++ static/js/socket.js
@@ -292,8 +292,6 @@ function connect() {
       if (confirm(msg + "\n\nKliknij OK żeby wrócić do strony głównej.")) {
         safeNavigateHome();
       }
-      if (inlineJoin) inlineJoin.style.display = "block";
-      if (chatSection) chatSection.style.display = "none";
       return;
     }
     // If we initiated a manual leave, do not auto-reconnect
